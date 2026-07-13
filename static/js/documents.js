6d996cd9291c452c798cb8575dcffd39b7cf439b
Ehab AlQabbani <eehab_said@hotmail.com>
2026-07-13 11:24:37 +0300

    modified document tab in the fixed assets to collaps and expand

--- static/js/documents.js
+++ static/js/documents.js
@@ -51,7 +51,7 @@
 
         return `
           <div class="item-card card" id="doc-card-${id}">
-            <div class="item-header card-header" onclick="toggleCollapsibleCard(this.closest('.item-card'), '#documentsListContainer')">
+            <div class="item-header card-header">
               <div class="item-header-left">
                 <svg class="item-chevron" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M9 18l6-6-6-6"/></svg>
                 <span class="item-category-badge">${category}</span>
